1264368db7fd95008e367948aac58cd8cca99583
Enriquepardo <quiquepardos@gmail.com>
2025-02-02 13:37:58 +0100

    .

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,18 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: enpardo- <enpardo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/24 20:32:46 by enpardo-          #+#    #+#             */
-/*   Updated: 2025/02/02 13:25:53 by enpardo-         ###   ########.fr       */
+/*   Created: 2025/02/02 13:10:43 by enpardo-          #+#    #+#             */
+/*   Updated: 2025/02/02 13:11:26 by enpardo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-char **ft_split(char const *s, char c)
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	
 }
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,18 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: enpardo- <enpardo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/24 20:32:46 by enpardo-          #+#    #+#             */
-/*   Updated: 2025/02/02 13:25:53 by enpardo-         ###   ########.fr       */
+/*   Created: 2025/02/02 13:09:48 by enpardo-          #+#    #+#             */
+/*   Updated: 2025/02/02 13:09:58 by enpardo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-char **ft_split(char const *s, char c)
+char *ft_strmapi(char const *s, char (*f)(unsigned
+int, char))
 {
-	
+
 }
