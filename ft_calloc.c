51cf2cc0d10fad0599f953ae884c36aa0cd87cfb
Enrique Pardo Serrano <enpardo-@c3r9s3.42madrid.com>
2025-02-04 19:16:50 +0100

    Libc functions

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: enpardo- <enpardo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 19:13:17 by enpardo-          #+#    #+#             */
-/*   Updated: 2025/02/03 21:30:21 by enpardo-         ###   ########.fr       */
+/*   Updated: 2025/02/04 18:54:34 by enpardo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,16 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*pt;
+	size_t	total_size;
 
 	if (nmemb == 0 || size == 0)
-	{
-		nmemb = 1;
-		size = 1;
-	}
+		return (NULL);
+	else
+		total_size = nmemb * size;
 	pt = malloc(nmemb * size);
 	if (!pt)
 		return (NULL);
-	return (ft_memset(pt, 0, nmemb * size));
+	if (total_size > 0)
+		ft_memset(pt, 0, total_size);
+	return (pt);
 }
