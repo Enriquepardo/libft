9a1b158a728108da278f17d63c2707809118230d
Enriquepardo <quiquepardos@gmail.com>
2025-02-18 19:56:47 +0100

    Libc functions

--- ft_substr.c
+++ ft_substr.c
@@ -6,34 +6,34 @@
 /*   By: enpardo- <enpardo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 20:45:25 by enpardo-          #+#    #+#             */
-/*   Updated: 2025/02/18 19:47:57 by enpardo-         ###   ########.fr       */
+/*   Updated: 2025/02/18 19:56:25 by enpardo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    char    *sub;
-    size_t  i = 0;
-    size_t  s_len;
+	char	*sub;
+	size_t	i;
+	size_t	s_len;
 
-    if (!s)
-        return (NULL);
-    s_len = ft_strlen(s);
-    if (start >= s_len)
-        return (ft_strdup(""));
-    if (len > s_len - start)
-        len = s_len - start;
-    sub = (char *)malloc(len + 1);
-    if (!sub)
-        return (NULL);
-    while (i < len && s[start + i])
-    {
-        sub[i] = s[start + i];
-        i++;
-    }
-    sub[i] = '\0';
-    return (sub);
+	i = 0;
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
+	sub = (char *)malloc(len + 1);
+	if (!sub)
+		return (NULL);
+	while (i < len && s[start + i])
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
 }
-
